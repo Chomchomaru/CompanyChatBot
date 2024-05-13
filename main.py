cb75bd75a4635c7fee13fe42162f1b5ff283c795
Chomchomaru <zamin.khan2611@gmail.com>
2024-05-13 23:37:44 +0400

    Fixed the pervious code made it more readable and added comments
    fixed the code in the main and trying to added a venv so its to use
    
    Co-Authored-By: myshkash <152983191+myshkash@users.noreply.github.com>

--- main.py
+++ main.py
@@ -9,48 +9,85 @@
 #7.give output X
 #8.continue the conversation until the user types 0 X
 
+APIKEY='llx-Zv1Ene56xuMc6S16xjUsom0YsGxvMNDrmJUjmFrpIF4tfvI2'
+
 #import libaraies
-from langchain_community.document_loaders import UnstructuredURLLoader
-from langchain_community.document_loaders import WebBaseLoader
-from langchain_community.document_loaders import SeleniumURLLoader
-from bs4 import BeautifulSoup as Soup
 from langchain_community.llms import Ollama
 from langchain_community.embeddings import OllamaEmbeddings
-from langchain_community.vectorstores import FAISS
-from operator import itemgetter
+from llama_parse import LlamaParse
+from llama_index.core import SimpleDirectoryReader
 
-#responses
-def getoutput(x):
-    response=(retriever | model).invoke(x)
-    return response.context
+parser=LlamaParse(
+    api_key=APIKEY,
+    result_type='markdown'
+)
 
 
-
+#information on the model, company and email
 MODEL='llama3:latest'
-
 model=Ollama(model=MODEL)
 embeddings = OllamaEmbeddings(model="nomic-embed-text",show_progress=True)
+company_name="GRG"
+mail="zamin.khan2611@gmail.com"
 
-urls = [
-    'https://grgmea.com',
-    'https://grgmea.com/talent-acquisition-10-step-work-flow/'
+from langchain.docstore.document import Document
+#get the Data the data from the 4 pdf proved
+#Llamaparse to parse the pdf
+def read_data():
+    reader = SimpleDirectoryReader(input_dir="./data")
+    documents=reader.load_data()
 
-]
+    #change the format of the documnet to make it work
+    docs=[Document(page_content=d.text )for d in documents]
+    
+    return docs
 
-loader = WebBaseLoader(urls)
-docs = loader.load_and_split()
+#take the document and the embedding model and embed and safe it in a db(for noew locally)
+#create a vector db 
+def embed():
+    from langchain_community.vectorstores import FAISS
+    doc=read_data()
+    vec=FAISS.from_documents(doc,embeddings)
+    return vec
 
-vector=FAISS.from_documents(docs,embeddings)
-retriever= vector.as_retriever()
+#get the most similar vectors from the vector db 
+#get the similar results
+def similarity_search(vec,input):
+    retervier=vec.as_retriever()
+    ss=retervier.invoke(input)
+    return ss
 
-def user_interaction():
-    question=''
-    print(getoutput("hello what is grgmea?"))
+#use langchain to prompt engineer for the desired out
+#get the template from this function
+def create_template(input,similarity_results):
+    from langchain.prompts import PromptTemplate
 
-user_interaction()
+    Template=""" You are a virtual assistant in {company_name} company and are responsible for answer there question. Do not deviate to other topics which 
+    are no related to the topic keep the answer short and clear and if there is no information regarding there enquire direct them to this mail:{mail}.
+    here are some context and the main question guven by the user
+    
+    
+    Context:{context}
+    Question:{question}
+    """
 
-#retriever.invoke(input)
+    prompt=PromptTemplate.from_template(Template)
+    return prompt.format(company_name=company_name,mail=mail,context=similarity_results,question=input)
 
+#ask the LLM about the query
+def ask_llm(context):
+    result=model.invoke(context)
+    return result 
 
-##from 5 to 8 Conversation with the chatbot
+#the entry point
+def chat(inputs):
+    vecdb=embed()
+    similarity_results=similarity_search(vecdb,inputs)
+    ct=create_template(inputs,similarity_results)
+    print(ask_llm(ct))
 
+while True:
+    i=input("Ask Mish about {company_name}?  ")
+    if(i!='0'):
+        chat(i)
+    else: break
